3f188177500ba292cc2b04fba8ce1f245070820d
matsushin <matsushin.dev@gmail.com>
2019-07-18 00:06:13 +0900

    RailsAdmiを導入

--- todo-api/config/routes.rb
+++ todo-api/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   namespace :api do
     namespace :v1 do
       resources :tasks, only: %i[index create update destroy]
