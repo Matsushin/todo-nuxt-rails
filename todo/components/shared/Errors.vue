b96bf520e02152ee6e30f22819a8c7ff183e68fc
matsushin <matsushin.dev@gmail.com>
2019-07-18 17:25:40 +0900

    エラー表示の見た目調整

--- todo/components/shared/Errors.vue
+++ todo/components/shared/Errors.vue
@@ -1,17 +1,12 @@
 <template>
-  <div
-    v-if="errors.length"
-    class="notice error">
-    <h2
-      v-if="showLabel"
-      class="notice-title">エラー:</h2>
-    <ul :class="{ 'notice-inline': !showLabel }">
-      <li
-        v-for="error in errors"
-        :key="error.id">
-        {{ error }}
-      </li>
-    </ul>
+  <div v-if="errors.length" class="mt-15">
+    <el-alert
+      title="エラー"
+      type="error"
+      :closable="false"
+      :description="errors.join('。')"
+      show-icon>
+    </el-alert>
   </div>
 </template>
 
