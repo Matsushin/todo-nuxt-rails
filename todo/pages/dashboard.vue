b262609b37bf492bd0b7da2bfd48bea186cb6083
matsushin <matsushin.dev@gmail.com>
2019-07-18 21:45:24 +0900

    マイページ微修正

--- todo/pages/dashboard.vue
+++ todo/pages/dashboard.vue
@@ -2,13 +2,20 @@
   <section class="container">
     <div>
       <h2>マイページ</h2>
+      <div class="tip">
+        <p>こんにちわ<b>{{currentUser.username}}</b>さん。<br>タスクを登録して日々のやることを忘れないようにしよう！！</p>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
-
-export default {
-
-}
+  import { mapGetters } from 'vuex'
+  export default {
+    computed: {
+      ...mapGetters({
+        currentUser: 'auth/currentUser'
+      })
+    }
+  }
 </script>
